51227cc719168ceeb8338fa6c7cbbd4ed7234680
Nico <nigr21@inf.ufpr.br>
2023-10-28 19:36:20 -0300

    Added alternative to stream play

--- src/Audio.py
+++ src/Audio.py
@@ -1,26 +1,43 @@
+import heapq
+import threading
+
 import sounddevice as sd
-from pydub import AudioSegment
-import numpy as np
+import soundfile as sf
 
-# Load the MP3 file
-audio = AudioSegment.from_file("music.mp3", format="mp3")
+filename = "music.mp3"
+blocksize = 2048
+h = []
+event = threading.Event()
 
-# Convert the audio to raw PCM format
-audio_data = np.array(audio.get_array_of_samples(), dtype=np.int16)
-sample_width = audio.sample_width
-frame_rate = audio.frame_rate
-channels = audio.channels
-
-# Define a callback function to play the audio
 def callback(outdata, frames, time, status):
-    global audio_data, sample_width, frame_rate, channels
-    print(status)
-    if len(audio_data) > 0:
-        chunk_size = frames * sample_width
-        outdata[:] = audio_data[:chunk_size]
-        audio_data = audio_data[chunk_size:]
+    try:
+        data = h[0][1]
+        heapq.heappop(h)
+    except IndexError:
+        print("Heap is empty, cannot pop an element.")
+    if len(data) < len(outdata):
+        outdata[:len(data)] = data
+        outdata[len(data):] = b'\x00' * (len(outdata) - len(data))
+        raise sd.CallbackStop
+    else:
+        outdata[:] = data
 
-# Start audio playback
-with sd.OutputStream(callback=callback, channels=channels, dtype="int16", samplerate=frame_rate):
-    pass
-
+try:
+    with sf.SoundFile(filename) as f:
+        i = 0
+        while True:
+            data = f.buffer_read(blocksize, dtype='float32')
+            if not data:
+                break
+            heapq.heappush(h, (i, data))  # Pre-fill queue
+            i += 1
+        stream = sd.RawOutputStream(
+            samplerate=f.samplerate, blocksize=blocksize,
+            device=None, channels=f.channels, dtype='float32',
+            callback=callback, finished_callback=event.set)
+        with stream:
+            event.wait()  # Wait until playback is finished
+except KeyboardInterrupt:
+    print('\nInterrupted by user')
+except Exception as e:
+    print(type(e).__name__ + ': ' + str(e))
