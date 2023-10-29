eece7fb745b854243e13685c0c1f73d5bf888cc7
Nico <nico.igramos@gmail.com>
2023-10-29 20:15:26 -0300

    Started to refactor utils

--- src/utils/Utils.py
+++ src/utils/Utils.py
@@ -1,6 +1,21 @@
 # Auxiliar functions for the project
 
-def start_threads(callback, args_callback = ''):
+import struct
+import threading
+
+def serialize_str(stri):
+    data = struct.pack('Q', len(stri))
+    data += stri.encode("utf-8")
+    return data
+
+def deserialize_str(packet):
+    length = struct.unpack('Q', packet[:8])[0]
+    packet = packet[8:]
+    msg = packet[:length].decode("utf-8")
+    packet = packet[length:]
+    return msg, packet
+
+def start_thread(callback, args_callback = ''):
     thread = threading.Thread(target=callback, args=(args_callback, ))
     thread.start()
     return thread
