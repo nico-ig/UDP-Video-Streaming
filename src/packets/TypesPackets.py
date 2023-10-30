5190065ef0f82a825d3cb3bdb6c921861932cfc2
Nico <nico.igramos@gmail.com>
2023-10-29 23:49:30 -0300

    Added types packets

---
+++ src/packets/TypesPackets.py
@@ -0,0 +1,8 @@
+"""
+Packets types and it's types value
+"""
+
+NEW_PORT_REQUEST = 1    # Client -> Server
+PORT_ALLOCATED = 2      # Server -> Client
+PORT_ACK = 3            # Client -> Server
+REGISTER = 4            # Client -> Server
