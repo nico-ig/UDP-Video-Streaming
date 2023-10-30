dcdecad4dd0063c8de5c32bd413d9f0efa91aa2e
Nico <nico.igramos@gmail.com>
2023-10-30 16:40:54 -0300

    Added comentaries

--- src/client/StreamClient.py
+++ src/client/StreamClient.py
@@ -1,3 +1,4 @@
+from http import server
 import streamHeap
 
 def print_available_musics(music_list):
@@ -10,16 +11,43 @@ def music_choices():
     print("You chose music ID: " + music_id)
     return music_id
 
-def add_stream_packet(stream_player, packet):
+def add_stream_packet(data, source):
+    if source != server:
+        return
+    
     key = packet[1]
     data = packet[2]
 
     stream_player.add_to_stream(key, data)
 
 
+def start_listening_stream():
+    network.register_callback(STREAM_PACKET, add_stream_packet)
+    play_heap()
+    
 
+0x5 0x00 0x00 0x00 0x00 0x00 0x2 0x1 0x0
 
+id[0] = packet[0]
+packet = packet[1:]
+tam = packet[0]
+packet = packet[1:]
+nome[0] = packet[:tam]
+music[0] = (id[0], name[0])
 
+list_received = threading.Event()
+
+def parse:
+    //parse pra music_list
+    list_received.set()
+
+def musicListFuntion:
+    register_callback(parse)
+    
+    while not list_received.is_set():
+        pass
+    
+    print_music_list()
 
 
 #### Dont forget to unregister from other packet types no longer relevants
