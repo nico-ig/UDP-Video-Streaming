091c8826dfbca4e421151a88c654276a1974ae74
Nico <nico.igramos@gmail.com>
2023-10-29 23:48:58 -0300

    Added client packets

--- src/packets/ClientPackets.py
+++ src/packets/ClientPackets.py
@@ -1,8 +1,1 @@
 # Deals with the parsing of the packets (incoming and outgoing)
-def mount_new_port_request_packet():
-    packet = bytes([1])
-    return packet
-
-def mount_port_ok_packet():
-    packet = bytes([3])
-    return packet
