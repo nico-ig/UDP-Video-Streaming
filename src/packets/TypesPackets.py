b66a0605b699c2bb4979b0ea8754a9747e073a11
Nico <nico.igramos@gmail.com>
2023-10-30 03:11:09 -0300

    Updated types packets

--- src/packets/TypesPackets.py
+++ src/packets/TypesPackets.py
@@ -6,3 +6,4 @@ NEW_PORT_REQUEST = 1    # Client -> Server
 PORT_ALLOCATED = 2      # Server -> Client
 PORT_ACK = 3            # Client -> Server
 REGISTER = 4            # Client -> Server
+REGISTER_ACK = 5        # Server -> Client
