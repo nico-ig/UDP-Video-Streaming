f2923f88cb5ecdd81cc27ac48324ff42425622d6
Nico <nico.igramos@gmail.com>
2023-10-29 23:48:43 -0300

    Added client main

--- src/client/MainClient.py
+++ src/client/MainClient.py
@@ -6,32 +6,30 @@ import signal
 import sys
 
 from src.client import HandshakeClient
+from src.client import GlobalClient
 from src.utils import Timer
 from src.utils import Logger
 from src.network import Network
 
-network = None
-server_timer = None
-server_timeout = 5
-server = None
+GlobalClient.LOGGER = Logger.start_logger()
+GlobalClient.LOGGER = Logger.get_logger('client')
 
-logger = Logger.start_logger()
-logger = Logger.get_logger('client')
 
 def sigint_handler(signum=0, frame=''):
     """
     Stops the client when sigint is received
     """
     try:
-        if server_timer != None:
-            server_timer.stop()
+        if GlobalClient.SERVER_TIMER != None:
+            GlobalClient.SERVER_TIMER.stop()
 
-        if network != None:
-            network.stop()
+        if GlobalClient.NETWORK != None:
+            GlobalClient.NETWORK.stop()
 
-        logger.info("Sigint received")
+        GlobalClient.LOGGER.info("Sigint received")
     except:
         pass
+
 
 def main():
     """
@@ -42,27 +40,27 @@ def main():
             print("Usage: python Client.py <Server Name> <Server Port> -j")
             exit()
 
-        logger.info("Creating client")
-        
-        signal.signal(signal.SIGINT, sigint_handler)
+        GlobalClient.LOGGER.info("Creating client")
 
-        global host, port, network, server_timer, server
+        signal.signal(signal.SIGINT, sigint_handler)
 
         server_name = sys.argv[1]
         server_port = sys.argv[2]
-        server = (server_name, server_port)
-        
-        network = Network.Network()
-        logger.info("Network interface created")
-        
-        server_timer = Timer.Timer(server_timeout, sigint_handler)
-        logger.info("Server timer initiated")
+        GlobalClient.SERVER = (server_name, server_port)
 
-        option = 'join' if len(sys.argv) == 4 and sys.argv[3] == "-j" else 'new'
-        HandshakeClient.client_handshake(network, server, server_timer, logger, option)
+        GlobalClient.NETWORK = Network.Network()
+        GlobalClient.LOGGER.info("GlobalClient.NETWORK interface created")
+
+        GlobalClient.SERVER_TIMER = Timer.Timer(
+            GlobalClient.SERVER_TIMEOUT, sigint_handler)
+        GlobalClient.LOGGER.info("Server timer initiated")
+
+        option = 'join' if len(
+            sys.argv) == 4 and sys.argv[3] == "-j" else 'new'
+        HandshakeClient.client_handshake(option)
 
     except Exception as e:
-        error_message = 'Error creating client - ' + type(e)+ ': ' + str(e)
-        logger.error(error_message)
+        GlobalClient.LOGGER.error("An error occurred: %s", str(e))
+
 
 main()
