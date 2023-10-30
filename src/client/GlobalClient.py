7d74b756c904a86065ccc5fea5ecaebfa781c86d
Nico <nico.igramos@gmail.com>
2023-10-29 23:49:14 -0300

    Added global values for client

---
+++ src/client/GlobalClient.py
@@ -0,0 +1,10 @@
+"""
+Global values for the client
+"""
+
+NETWORK = None          # Network interface
+SERVER = None           # Server address
+SERVER_TIMER = None     
+LOGGER = None           
+
+SERVER_TIMEOUT = 5      # Time in seconds before timeouting when not receiving packets from server
