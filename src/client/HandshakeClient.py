f05decfd2cfc48d6a1ca9a1ac645b4a6f62ac8d1
Nico <nico.igramos@gmail.com>
2023-10-29 23:48:24 -0300

    Refactored client handshake

--- src/client/HandshakeClient.py
+++ src/client/HandshakeClient.py
@@ -1,29 +1,79 @@
+"""
+Manages the handshake with the server
+"""
 
-from src.packets import ClientPackets
+from src.packets import UtilsPackets
+from src.packets import TypesPackets
+from src.client import GlobalClient
 
-def send_registration_packet(destination):
-    packet = bytes([4])
-    print(f"Sending packet {packet} to {destination}")
-    network.send(destination, packet)
+
+def send_registration_packet(server):
+    """
+    Send to server a request to register at a stream
+    """
+    try:
+        packet = UtilsPackets.mount_byte_packet(TypesPackets.REGISTER)
+
+        GlobalClient.LOGGER.info("Sending register to %s", GlobalClient.SERVER)
+        GlobalClient.NETWORK.send(server, packet)
+    
+    except Exception as e:
+        GlobalClient.LOGGER.error("An error occurred: %s", str(e))
+
 
 def port_allocated(data, source):
-    if len(data) != 0:
-        return
+    """
+    Callback function when receiving the port allocated from server. Answer with an port ack
+    """
+    try:
+        if len(data) != 0:
+            return
 
-    server_timer.kick()
-    packet = ClientPackets.mount_port_ok_packet()
-    network.send(source, packet)
-##### Desrregistrar o tipo 2
-    send_registration_packet(source)
+        GlobalClient.LOGGER.info("Received port allocated from %s", source)
 
-def send_port_request(network, server):
-    packet = ClientPackets.mount_new_port_request_packet()
-    network.send(server, packet)
+        GlobalClient.SERVER_TIMER.kick()
+        GlobalClient.NETWORK.unregister_callback(TypesPackets.PORT_ALLOCATED)
 
-def client_handshake(network, server, server_timer, logger, option):
-    if option == 'join':
-        send_registration_packet(network, server)
+        packet = UtilsPackets.mount_byte_packet(TypesPackets.PORT_ACK)
 
-    else:
-        send_port_request(network, server)
-        network.register_callback(2, port_allocated)
+        GlobalClient.LOGGER.info("Sending port ack to %s", GlobalClient.SERVER)
+        GlobalClient.NETWORK.send(source, packet)
+
+        send_registration_packet(source)
+
+    except Exception as e:
+        GlobalClient.LOGGER.error("An error occurred: %s", str(e))
+
+
+def send_port_request():
+    """
+    Send the request for a stream port at the server
+    """
+    try:
+        packet = UtilsPackets.mount_byte_packet(TypesPackets.NEW_PORT_REQUEST)
+
+        GlobalClient.LOGGER.info("Sending new port request to %s", GlobalClient.SERVER)
+        GlobalClient.NETWORK.send(GlobalClient.SERVER, packet)
+
+        GlobalClient.NETWORK.register_callback(
+            TypesPackets.PORT_ALLOCATED, port_allocated)
+
+    except Exception as e:
+        GlobalClient.LOGGER.error("An error occurred: %s", str(e))
+
+
+def client_handshake(option):
+    """
+    Perform the handshake with the server
+    """
+    try:
+        if option == 'join':
+            GlobalClient.LOGGER.info("Joining stream at %s", GlobalClient.SERVER)
+            send_registration_packet(GlobalClient.SERVER)
+
+        else:
+            GlobalClient.LOGGER.info("Entering new stream")
+            send_port_request()
+
+    except Exception as e:
+        GlobalClient.LOGGER.error("An error occurred: %s", str(e))
