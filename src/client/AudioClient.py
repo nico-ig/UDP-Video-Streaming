1b988df73a2f8fc5a22d553af33487d83088b53f
Nico <nico.igramos@gmail.com>
2023-11-03 19:46:01 -0300

    Trying to capture alsa underrun error but not working

--- src/client/AudioClient.py
+++ src/client/AudioClient.py
@@ -1,6 +1,8 @@
 # Deals with audio input 
 # Recieves stream of audio and plays it 
 
+import os
+import shutil
 import heapq
 import threading
 import struct
@@ -12,34 +14,58 @@ from src.utils import Packets
 from src.utils import Utils
 
 file_path = "musics"
-file_blocksize = 2048   # Power of two that fits in MTU = 1500
+file_blocksize = 1024
 h = []
 event = threading.Event()
+heap_lock = threading.Lock()
 
 def callback(outdata, frames, time, status):
-    data = h[0][1]
-    heapq.heappop(h)
-    print(data)
-    if len(data) < len(outdata):
-        outdata[:len(data)] = data
-        outdata[len(data):] = b'\x00' * (len(outdata) - len(data))
     try:
-        data = h[0][1]
-        print(f"Playing segment {h[0][0]}")
-        heapq.heappop(h)
-        if len(data) < len(outdata):
-            outdata[:len(data)] = data
-            outdata[len(data):] = b'\x00' * (len(outdata) - len(data))
+        with heap_lock:
+            seq, stream = heapq.heappop(h)
+            
+        if len(stream) < len(outdata):
+            outdata[:len(stream)] = stream
+            outdata[len(stream):] = b'\x00' * (len(outdata) - len(stream))
+
             raise sd.CallbackStop
+
         else:
-            outdata[:] = data
+            outdata[:] = stream
     except:
         raise sd.CallbackStop
-    else:
-        outdata[:] = data
     
 player = StreamHeap.stream_player()
  
+def capture_alsa():
+    try:
+        if os.path.isfile('~/.asoundrc'):
+            shutil.copy('~/.asoundrc', '~/.asoundrc_bkp')
+
+        os.system('cat ./config/asoundrc >> ~/.asoundrc')
+
+        with open('~/.asoundrc', "rw") as config_file:
+            alsa_config = config_file.read()
+            output_device = sd.query_devices(kind='output', query='default')
+            print(alsa_config)
+            alsa_config = alsa_config.replace("<device>", output_device)
+            print(alsa_config)
+            config_file.write(alsa_config)
+
+    except:
+        pass
+
+def restore_alsa():
+    try:
+        if os.path.isfile('~/.asoundrc_bkp'):
+            shutil.copy('~/.asoundrc_bkp', '~/.asoundrc')
+            os.remove('~/.asoundrc_bkp')
+        else:
+            os.remove('~/.asoundrc')
+
+    except:
+        pass
+
 def reproduce_stream(): # Call this function after the "STREAM_PACKET" is received
     try:
         packet = []
@@ -63,24 +89,33 @@ def reproduce_stream(): # Call this function after the "STREAM_PACKET" is receiv
             channels = struct.unpack('Q', config)[0]
             print(f"Channles: {channels}")
 
-            for stream in audio_streams:
-                key = struct.unpack('Q', stream[:8])[0]
-                stream = stream[8:]
-                print(stream)
-                heapq.heappush(h, (key, stream))
-            
+            #capture_alsa()
+            player = sd.RawOutputStream(
+                samplerate=sample_rate, blocksize=file_blocksize // 8,
+                device=None, channels=channels, dtype='float32',
+                callback=callback, finished_callback=event.set)
 
-            
-        stream = sd.RawOutputStream(
-            samplerate=sample_rate, blocksize=file_blocksize // 8,
-            device=None, channels=channels, dtype='float32',
-            callback=callback, finished_callback=event.set)
-        with stream:
-            event.wait()  # Wait until playback is finished
+
+            for stream in audio_streams:
+                seq = struct.unpack('Q', stream[:8])[0]
+                stream = stream[8:]
+                # Não sei e precisa, mas é pra evitar adicionar um pacote com a callback fazendo o pop
+                with heap_lock:
+                    heapq.heappush(h, (seq, stream))
+
+            with player:
+                event.wait()  # Wait until playback is finished
+                print("Finished")
+                event.clear()
+
     except KeyboardInterrupt:
         print('\nInterrupted by user')
+
     except Exception as e:
         print(type(e).__name__ + ': ' + str(e))
 
-
+    # finally:
+    #     restore_alsa()
+        
+        
 reproduce_stream()
